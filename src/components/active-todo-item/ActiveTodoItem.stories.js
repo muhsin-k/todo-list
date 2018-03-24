184c15ca83e75a09e508324f97272c0595826f46
MUHSIN K <muhzi4u@gmail.com>
2018-03-24 10:11:25 +0530

    feat: code beautification

--- src/components/active-todo-item/ActiveTodoItem.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import StoryRouter from 'storybook-router';
-
-import ActiveTodoItem from './ActiveTodoItem';
-
-storiesOf('ActiveTodoItem', module)
-  .addDecorator(StoryRouter())
-  .add('with Link', () => <ActiveTodoItem />);
--- src/components/complete-todo-item/CompleteTodoItem.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import StoryRouter from 'storybook-router';
-
-import CompleteTodoItem from './CompleteTodoItem';
-
-storiesOf('CompleteTodoItem', module)
-  .addDecorator(StoryRouter())
-  .add('with Link', () => <CompleteTodoItem />);
