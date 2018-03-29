2c1680500010a60419413c0e3cdf02644ec7b7ab
MUHSIN K <muhzi4u@gmail.com>
2018-03-29 13:21:38 +0530

    bf

--- todo-backend/services/passport.js
+++ todo-backend/services/passport.js
@@ -21,7 +21,7 @@ passport.use(
     {
       callbackURL: "/auth/google/callback",
       clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret
+      clientSecret: keys.googleClientSecret,
       proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
