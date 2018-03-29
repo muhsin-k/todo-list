554e41109a826389399923f756822cca16f1689d
MUHSIN K <muhzi4u@gmail.com>
2018-03-29 13:22:20 +0530

    bf

--- todo-backend/routes/User.js
+++ todo-backend/routes/User.js
@@ -10,24 +10,26 @@ module.exports = app => {
     })
   );
 
-  // app.get(
-  //   "/auth/google/callback",
-  //   passport.authenticate("google"),
-  //   (req, res) => {
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      setTimeout(function() {
+        res.redirect("http://localhost:3000/home");
+      }, 2000);
+    }
+  );
+  // app.get("/auth/google/callback", passport.authenticate("google"), function(
+  //   req,
+  //   res
+  // ) {
+  //   // Explicitly save the session before redirecting!
+  //   console.log("req.session", req.session);
+  //   req.session.save(() => {
+  //     console.log("Save session");
   //     res.redirect("http://localhost:3000/home");
-  //   }
-  // );
-  app.get("/auth/google/callback", passport.authenticate("google"), function(
-    req,
-    res
-  ) {
-    // Explicitly save the session before redirecting!
-    console.log("req.session", req.session);
-    req.session.save(() => {
-      console.log("Save session");
-      res.redirect("http://localhost:3000/home");
-    });
-  });
+  //   });
+  // });
 
   app.get("/api/user", async (req, res) => {
     res.status(200).send(req.user);
