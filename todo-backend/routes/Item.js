3ad12b6d9a0ff4f542c50b876f326a248a7eca75
MUHSIN K <muhzi4u@gmail.com>
2018-03-29 12:19:10 +0530

    bf

--- todo-backend/routes/Item.js
+++ todo-backend/routes/Item.js
@@ -12,7 +12,7 @@ module.exports = app => {
         });
       res.status(200).json(items);
     } catch (err) {
-      res.status(401).json(err);
+      res.status(404).json(err);
     }
   });
   app.put("/api/item", cleanCache, async (req, res) => {
