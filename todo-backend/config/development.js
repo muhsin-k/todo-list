db3fadb0e061482eb277056afc53ac9166225612
MUHSIN K <muhzi4u@gmail.com>
2018-03-29 11:51:25 +0530

    fix: update google ids

--- todo-backend/config/development.js
+++ todo-backend/config/development.js
@@ -3,11 +3,11 @@
 module.exports = {
   mongoURI: "mongodb://localhost/express-starter",
   //My Ids
-  // googleClientID:
-  //   "274591579725-agob6nvha0brikc82h0v9i29nth19oje.apps.googleusercontent.com",
-  // googleClientSecret: "UFQC44YVGB-ajIocgBzhkpbE",
   googleClientID:
-    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
-  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+    "274591579725-agob6nvha0brikc82h0v9i29nth19oje.apps.googleusercontent.com",
+  googleClientSecret: "UFQC44YVGB-ajIocgBzhkpbE",
+  // googleClientID:
+  //   "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+  // googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
   cookieKey: "123123123"
 };
